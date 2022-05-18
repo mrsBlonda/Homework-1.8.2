3c3f9777c696c26a0b900207345f9023b29915b9
Прохватаева Анна <Anna89851004453@yandex.ru>
2022-05-18 22:56:31 +0300

    Изменения в файлах

--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -10,28 +10,32 @@ public class Server {
     public static void main(String[] args) throws IOException {
         System.out.println("Начало соединения");
         int port = 8079;
-        ServerSocket serverSocket = new ServerSocket(port);
-        while (true) {
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            System.out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
+        try (ServerSocket serverSocket = new ServerSocket(port);) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
+                    System.out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
 
-            out.println("Write your name");
+                    out.println("Write your name");
 
-            final String name = in.readLine();
+                    final String name = in.readLine();
 
+                    out.println("Are you child? (yes/no)");
 
-            out.println("Are you child? (yes/no)");
+                    String age = in.readLine();
 
-            String age = in.readLine();
-
-            if (age.equals("yes")) {
-                out.println("Welcome to the kids area, " + name + " Let's play!");
-            } else {
-                out.println("Welcome to the adult zone, " + name +
-                        " Have a good rest, or a good working day!");
+                    if (age.equals("yes")) {
+                        out.printf("Welcome to the kids area, %s Let's play!", name);
+                    } else {
+                        out.printf("Welcome to the adult zone, %s Have a good rest, or a good working day!", name);
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
